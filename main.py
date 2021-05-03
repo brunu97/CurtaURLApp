06375b37e745cd446f3834ddd3eadceb6336b8b1
brunu97 <brunuo@live.com.pt>
2021-05-03 14:57:45 +0100

    Inicial

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from hashids import Hashids
+import uuid
+
+app = Flask(__name__)
+hashids = Hashids(min_length=4, salt=str(uuid.uuid1()))
+app.secret_key = "1ad1d379a20cb884"
+LISTA_URLS = {}
+
+
+@app.route('/erro', methods=['GET'])
+def naoEcontrado():
+    return render_template('erro.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        url_i = request.form['URL']
+        if len(url_i) > 0:
+            link_gerado = hashids.encode(len(LISTA_URLS))
+            LISTA_URLS[link_gerado] = url_i
+            session["url"] = request.host_url + "l" + link_gerado
+        return redirect(url_for('index'))
+    elif request.method == 'GET':
+        if "url" in session:
+            nova_url = session["url"]
+            return render_template('index.html', nova_url=nova_url)
+        return render_template('index.html')
+
+
+@app.route('/l<string:lnk>', methods=['GET'])
+def link(lnk):
+    try:
+        return redirect(LISTA_URLS[lnk])
+    finally:
+        return redirect(url_for('naoEcontrado'))
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return redirect(url_for('naoEcontrado'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
